a06d6ca1680b27742ae7b4cb7ca7995a0fd844d0
Harrison Yu <mr.harrisonyu@gmail.com>
2012-07-03 23:45:43 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FitnessGraph::Application.config.secret_token = '50ddc3063673f90b411b426d7730ae7b8f01c2393088329533b07c381ebbeda82dcf80941c845ff06915f480b39523717077492128bb36a04a60be70a70f4ec0'
