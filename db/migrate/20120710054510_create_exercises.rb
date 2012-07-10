e7c0136c6fc8d59e45bd20739f61e02d9a5bf545
Wynn Chen <wynnch@gmail.com>
2012-07-09 23:07:47 -0700

    added exercise table

---
+++ db/migrate/20120710054510_create_exercises.rb
@@ -0,0 +1,18 @@
+class CreateExercises < ActiveRecord::Migration
+  def self.up
+    
+    create_table :exercises do |t|
+      t.string :name
+      t.string :type
+      t.datetime :date
+
+      t.timestamps
+    end
+    
+  end
+  
+  def self.down
+    drop_table :exercises
+  end
+  
+end
